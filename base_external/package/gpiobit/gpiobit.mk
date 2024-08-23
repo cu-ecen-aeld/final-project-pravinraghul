a2f4702c371a46248560337d1f47221fc49814c3
pravinraghul <pravinraghul@gmail.com>
2024-08-23 20:06:19 +0530

    Added the python application into the buildroot

--- base_external/package/gpiobit/gpiobit.mk
+++ base_external/package/gpiobit/gpiobit.mk
@@ -1,7 +1,11 @@
-GPIOBIT_VERSION = cda244cbcdcdb441c9dc449a2f19f0790b876dfb
+GPIOBIT_VERSION = b77a55d85dfe94f48d25c155e675de474d462f23
 GPIOBIT_SITE = git@github.com:pravinraghul/ECEN-5713-Project.git
 GPIOBIT_SITE_METHOD = git
 GPIOBIT_MODULE_SUBDIRS += gpiobit-driver
+
+define GPIOBIT_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/rpi/app.py $(TARGET_DIR)/root
+endef
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
